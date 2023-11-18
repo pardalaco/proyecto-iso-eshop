2cfa10b99d5c84336104a670337d4a31d2c94c03
Pablo Segovia <psegmar1@epsa.upv.es>
2023-11-18 22:35:34 +0100

    Solo hecho barra busqueda, queda drawer y buscar por tag

--- src/client/eshop/lib/views/home.dart
+++ src/client/eshop/lib/views/home.dart
@@ -4,24 +4,29 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:eshop/sockets/connection.dart';
 import 'package:eshop/models/Product.dart';
+import 'package:eshop/style/ColorsUsed.dart';
 
 class Home extends StatefulWidget {
   Connection connection;
-  Home({Key? key, required this.connection}) : super(key: key);
+  bool admin;
+  Home({Key? key, required this.connection, required this.admin})
+      : super(key: key);
 
   @override
-  State<Home> createState() => _MyPage(connection: connection);
+  State<Home> createState() => _MyPage(connection: connection, admin: admin);
 }
 
 class _MyPage extends State<Home> {
   Connection connection;
-  _MyPage({required this.connection});
+  bool admin;
+  _MyPage({required this.connection, required this.admin});
 
   @override
   Widget build(BuildContext context) {
     final _MyAppBar = AppBar(
         toolbarHeight: MediaQuery.of(context).size.height * 0.1,
-        backgroundColor: Colors.amber,
+        backgroundColor: CustomColors.n1,
+        centerTitle: true,
         title: Text(
           "Home",
           style: TextStyle(
@@ -31,7 +36,82 @@ class _MyPage extends State<Home> {
         ));
     return Scaffold(
       appBar: _MyAppBar,
-      body: FuturaLista(context, connection),
+      body: _MyHomeBody(connection: connection, admin: admin),
+    );
+  }
+}
+
+class _MyHomeBody extends StatefulWidget {
+  Connection connection;
+  bool admin;
+  _MyHomeBody({Key? key, required this.connection, required this.admin});
+
+  @override
+  State<_MyHomeBody> createState() =>
+      _HomeBody(connection: connection, admin: admin);
+}
+
+class _HomeBody extends State<_MyHomeBody> {
+  Connection connection;
+  bool admin;
+  _HomeBody({required this.connection, required this.admin});
+  //TextEditingController controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  /*@override
+  void dispose() {
+    //controller.dispose();
+    super.dispose();
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsetsDirectional.symmetric(
+          horizontal: size.width * 0.05, vertical: size.height * 0.03),
+      color: CustomColors.background,
+      child: Column(
+        children: [
+          TextField(
+            //controller: controller,
+            focusNode: _focusNode,
+            decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(width: 3, color: CustomColors.n1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(width: 3, color: CustomColors.n1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                hintText: "Busca en la tienda",
+                filled: true,
+                fillColor: CustomColors.n2,
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: _focusNode.hasFocus ? Colors.grey : Colors.white,
+                )),
+            onSubmitted: (value) => {
+              setState(() {
+                String textoIngresado = value;
+                print('Texto ingresado: $textoIngresado');
+              })
+            },
+          ),
+          SizedBox(height: 20),
+          /*ElevatedButton(
+            onPressed: () {
+              // Recoger el texto del TextField
+              String textoIngresado = controller.text;
+              print('Texto ingresado: $textoIngresado');
+            },
+            child: Text('Recoger Texto'),
+          ),*/
+        ],
+      ),
     );
   }
 }
