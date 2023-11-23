07fe5bdc113ec3e8170032c1a1ee7dda47078089
Pablo Segovia <psegmar1@epsa.upv.es>
2023-11-23 23:25:04 +0100

    Main para testear desde Home

--- src/client/eshop/lib/main.dart
+++ src/client/eshop/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:eshop/models/Profile.dart';
 import 'package:flutter/material.dart';
 import 'views/logIn.dart';
+import 'views/home.dart';
+import 'sockets/connection.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +12,17 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
         title: 'eShop',
         debugShowCheckedModeBanner: false,
-        home: logIn(),
+        home: Home(
+            connection: Connection(),
+            admin: true,
+            profile: Profile(
+                email: "psegmar1@gmail.com",
+                name: "Pablo",
+                surname: "Segovia",
+                payments: ["PayPal"],
+                addresses: ["Calle Ave del paraiso"])),
       );
 }
