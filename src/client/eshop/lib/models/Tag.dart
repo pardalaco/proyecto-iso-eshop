f62f9d9b1db8d40f6abfb135a7ecefa691bec38a
Pablo Segovia <psegmar1@epsa.upv.es>
2023-12-07 20:44:52 +0100

    Nuevos modelos

---
+++ src/client/eshop/lib/models/Tag.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: file_names
+
+class Tags {
+  late List<Tag> tags;
+
+  Tags.fromJson(Map<String, dynamic> json) {
+    tags = <Tag>[];
+    var a = json['tags'];
+    a.forEach((v) {
+      tags.add(Tag.fromJson(v, false));
+    });
+  }
+
+  @override
+  String toString() {
+    String rv = "";
+    for (Tag t in tags) {
+      rv += "${t.name} = ${t.choose.toString()}";
+    }
+    return rv;
+  }
+}
+
+class Tag {
+  late String name;
+  late bool choose;
+
+  Tag.fromJson(this.name, this.choose);
+}
