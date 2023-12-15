8d640fe717078c8325b8a75a1588e85095ae66c6
Hiram Montejano Gomez <hiram.montejano.gomez@gmail.com>
2023-12-15 10:47:36 +0100

    updated with gui in progress

--- client/eshop/lib/main.dart
+++ client/eshop/lib/main.dart
@@ -1,27 +1,28 @@
+import 'package:eshop/models/Profile.dart';
 import 'package:flutter/material.dart';
-import "package:eshop/loginPage.dart";
-
-import 'package:eshop/sockets/connection.dart';
+import 'views/logIn.dart';
+import 'views/home.dart';
+import 'sockets/connection.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(connection: Connection()),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'eShop',
+        debugShowCheckedModeBanner: false,
+        home: Home(
+            connection: Connection(),
+            admin: true,
+            profile: Profile(
+                email: "psegmar1@gmail.com",
+                name: "Pablo",
+                surname: "Segovia",
+                payments: ["PayPal"],
+                addresses: ["Calle Ave del paraiso"])),
+      );
 }
