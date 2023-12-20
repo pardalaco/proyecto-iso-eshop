11eeb1eaaa7aea8c4ea3b425fdb5cbb254d99033
Pablo Segovia <psegmar1@epsa.upv.es>
2023-12-20 21:10:25 +0100

    Actualizar

--- client/eshop/lib/models/Tag.dart
+++ client/eshop/lib/models/Tag.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, non_constant_identifier_names
 
 class Tags {
   late List<Tag> tags;
@@ -7,7 +7,7 @@ class Tags {
     tags = <Tag>[];
     var a = json['tags'];
     if (fromProduct) {
-      a = a.split(",");
+      a = a?.isEmpty ?? true ? [] : a.split(",");
     }
     a.forEach((v) {
       tags.add(Tag.fromJson(v, false));
@@ -20,7 +20,7 @@ class Tags {
     for (Tag t in tags) {
       rv += "${t.name},";
     }
-    return rv.substring(0, rv.length - 1);
+    return rv.isNotEmpty ? rv.substring(0, rv.length - 1) : "";
   }
 
   String onlyTrues() {
@@ -30,12 +30,29 @@ class Tags {
         rv += "${t.name},";
       }
     }
-    return rv.substring(0, rv.length - 1);
+    return rv.isNotEmpty ? rv.substring(0, rv.length - 1) : "";
+  }
+
+  void intersectionToTrue(Tags p_tags) {
+    late int index;
+    for (Tag t in p_tags.tags) {
+      if ((index = tags.indexOf(t)) != -1) {
+        tags[index].choose = true;
+      }
+    }
   }
 }
 
 class Tag {
   late String name;
   late bool choose;
+
   Tag.fromJson(this.name, this.choose);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Tag && name == other.name;
+
+  @override
+  int get hashCode => name.hashCode;
 }
