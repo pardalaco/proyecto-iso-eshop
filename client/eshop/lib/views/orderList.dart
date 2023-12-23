08749318135df40f9c6babbe3f6b61adc523d610
Daniel Rovira Martínez <drovmar@upv.es>
2023-12-23 12:10:06 +0100

    OrderList: separar código por funciones

--- client/eshop/lib/views/orderList.dart
+++ client/eshop/lib/views/orderList.dart
@@ -11,12 +11,12 @@ class OrderList extends StatefulWidget {
   final Profile profile;
   final bool admin;
 
-  const OrderList(
-      {Key? key,
-      required this.connection,
-      required this.profile,
-      required this.admin})
-      : super(key: key);
+  const OrderList({
+    Key? key,
+    required this.connection,
+    required this.profile,
+    required this.admin,
+  }) : super(key: key);
 
   @override
   _OrderListState createState() => _OrderListState();
@@ -28,94 +28,107 @@ class _OrderListState extends State<OrderList> {
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
         backgroundColor: CustomColors.background,
-        appBar: AppBar(
-          toolbarHeight: constraints.maxHeight * 0.1,
-          backgroundColor: CustomColors.n1,
-          centerTitle: true,
-          title: Text(
-            "Order List",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: constraints.maxHeight * 0.05,
-            ),
-          ),
-        ),
-        body: FutureBuilder(
-          future: widget.connection.getAllCarts(widget.profile.email),
-          builder: (context, AsyncSnapshot<String> snapshot) {
-            if (snapshot.hasData) {
-              Response response = Response.fromJson(snapshot.data!);
-              if (response.error) {
-                return const Center(
-                  child: Text(
-                    "Something went wrong",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                );
-              } else {
-                Carts carts = Carts.fromJson(response.content);
-                if (carts.carts.isEmpty) {
-                  return const Center(
-                    child: Text(
-                      "There aren't any orders",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  );
-                } else {
-                  return ListView.builder(
-                    itemCount: carts.carts.length,
-                    itemBuilder: (context, index) {
-                      Cart cart = carts.carts[index];
-                      return ListTile(
-                        title: Text(
-                          cart.cartname,
-                          style: const TextStyle(
-                            fontSize: 25,
-                            color: Colors.white, // Agregar el color aquí
-                          ),
-                        ),
-                        subtitle: Text(
-                          cart.total.toStringAsFixed(2) + " €",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white, // Agregar el color aquí
-                          ),
-                        ),
-                        onTap: () async {
-                          // Implementa tu lógica para navegar a la vista de detalles de la orden
-                        },
-                      );
-                    },
-                  );
-                }
-              }
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation(CustomColors.n1),
-                ),
-              );
-            }
-          },
-        ),
-        floatingActionButton: widget.admin
-            ? FloatingActionButton(
-                onPressed: () async {
-                  // Aquí implementa la lógica para el botón
-                },
-                backgroundColor: CustomColors.n1,
-                child: const Icon(Icons.search, color: Colors.white),
-              )
-            : null,
+        appBar: buildAppBar(constraints),
+        body: buildOrderListBody(),
+        floatingActionButton: widget.admin ? buildSearchButton() : null,
       );
     });
+  }
+
+  AppBar buildAppBar(BoxConstraints constraints) {
+    return AppBar(
+      toolbarHeight: constraints.maxHeight * 0.1,
+      backgroundColor: CustomColors.n1,
+      centerTitle: true,
+      title: Text(
+        "Order List",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: constraints.maxHeight * 0.05,
+        ),
+      ),
+    );
+  }
+
+  Widget buildOrderListBody() {
+    return FutureBuilder(
+      future: widget.connection.getAllCarts(widget.profile.email),
+      builder: (context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.hasData) {
+          Response response = Response.fromJson(snapshot.data!);
+          if (response.error) {
+            return buildErrorWidget("Something went wrong");
+          } else {
+            Carts carts = Carts.fromJson(response.content);
+            if (carts.carts.isEmpty) {
+              return buildErrorWidget("There aren't any orders");
+            } else {
+              return buildOrderListView(carts);
+            }
+          }
+        } else {
+          return buildLoadingWidget();
+        }
+      },
+    );
+  }
+
+  Widget buildOrderListView(Carts carts) {
+    return ListView.builder(
+      itemCount: carts.carts.length,
+      itemBuilder: (context, index) {
+        Cart cart = carts.carts[index];
+        return ListTile(
+          title: Text(
+            cart.cartname,
+            style: const TextStyle(
+              fontSize: 25,
+              color: Colors.white, // Agregar el color aquí
+            ),
+          ),
+          subtitle: Text(
+            cart.total.toStringAsFixed(2) + " €",
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.white, // Agregar el color aquí
+            ),
+          ),
+          onTap: () async {
+            // Implementa tu lógica para navegar a la vista de detalles de la orden
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildErrorWidget(String errorMessage) {
+    return Center(
+      child: Text(
+        errorMessage,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget buildLoadingWidget() {
+    return const Center(
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(CustomColors.n1),
+      ),
+    );
+  }
+
+  FloatingActionButton buildSearchButton() {
+    return FloatingActionButton(
+      onPressed: () async {
+        // Aquí implementa la lógica para el botón
+      },
+      backgroundColor: CustomColors.n1,
+      child: const Icon(Icons.search, color: Colors.white),
+    );
   }
 }
